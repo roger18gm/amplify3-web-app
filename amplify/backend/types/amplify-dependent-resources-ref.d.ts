82d010d3bde4e5a722748174931a2f89df24f380
roger18gm <roger18gm@gmail.com>
2024-07-01 13:49:08 -0600

    test

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyapp3": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "amplifyapp3595aa57d": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { createNote, deleteNote} from './graphql/mutations'
+import { listNotes } from './graphql/queries'
+import { withAuthenticator, Button, Text, Flex, Heading } from "@aws-amplify/ui-react";
+import { useCallback, useEffect, useState } from 'react';
+import { Amplify } from 'aws-amplify';
+import { generateClient } from 'aws-amplify/api';
 
-function App() {
+const client = generateClient();
+
+function App({ signOut }) {
+  const [ notes, setNotes ] = useState([])
+
+  const fetchNotes = useCallback(async () => {
+    const result = await client.graphql({
+      query: listNotes,
+      authMode: 'AMAZON_COGNITO_USER_POOLS'
+    })
+    setNotes(result.data.listNotes.items)
+  }, [setNotes])
+
+  const handleCreateNote = useCallback(async () => {
+    await client.graphql({
+      query: createNote,
+      variables: { input: { text: window.prompt("New note") } },
+      authMode: 'AMAZON_COGNITO_USER_POOLS'
+    })
+    fetchNotes()
+  }, [fetchNotes])
+
+  const handleDeleteNote = useCallback(async (id) => {
+    await client.graphql({
+      query: deleteNote,
+      variables: { input: { id: id } },
+      authMode: 'AMAZON_COGNITO_USER_POOLS'
+    })
+    fetchNotes()
+  }, [fetchNotes])
+
+  useEffect(() => {
+    fetchNotes()
+  }, [fetchNotes])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex direction={"column"}>
+      <Flex justifyContent={'space-between'}>
+        <Heading level={1}>My notes</Heading>
+        <Button onClick={signOut}>Sign Out</Button>
+      </Flex>
+      {notes.map(note => <Flex alignItems={'center'}>
+        <Text>{note.text}</Text>
+        <Button onClick={() => handleDeleteNote(note.id)}>Remove</Button>
+      </Flex>)}
+      <Button onClick={handleCreateNote}>Add Note</Button>
+    </Flex>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
